de7050116b6c422017075950596a99cd1b3716d0
XW <xiaowen@edsz9.com>
2021-02-22 19:39:08 +0800

    添加代理

--- SocketDemo/SocketDemo/Socket.swift
+++ SocketDemo/SocketDemo/Socket.swift
@@ -8,7 +8,15 @@
 import UIKit
 import SocketIO
 
+protocol SocketDelegate: NSObjectProtocol {
+    /// 收到消息
+    func socketDidReceiveData()
+    /// 连接成功
+    func socketDidConnected()
+}
+
 class Socket: NSObject {
+    private weak var delegate: SocketDelegate?
     /// 单例
     private static let shared: Socket = {
         let instance = Socket()
